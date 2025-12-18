737519d7ae95fdad93769d1345ad74f5e28c82c5
devopssachin14 <sachinmr1407@gmail.com>
2025-12-18 12:01:04 +0530

    Refactor Factorial class into App class

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,10 @@
-public class Factorial {
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
     public static void main(String[] args) {
-        int number = 5;
-        long factorial = 1;
-
-        for (int i = 1; i <= number; ++i) {
-            factorial *= i; // Equivalent to factorial = factorial * i;
-        }
-
-        System.out.println("Factorial of " + number + " is: " + factorial);
+        System.out.println("Hello World!");
     }
 }
